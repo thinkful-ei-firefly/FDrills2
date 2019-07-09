90b8be0dc18267d7d197fb61e7dfc9f0275e02fb
Hector Valles <hmvalles75@yahoo.com>
2019-07-09 05:42:52 -0700

    drill5

--- index.js
+++ index.js
@@ -30,8 +30,11 @@ function decode(word) {
     }
 } //Answer = for loop!!!!
 
-function daysInMonth(month, leapYear) {
-
+function daysInMonth(month, year) {
+    if ((year % 100 === 0) ? (year % 400 === 0) : (year % 100 === 0)) && month = "February" {
+        console.log(`February has 29 days`);
+    }
+}
 switch (month) {
     
     case 'January':
@@ -56,4 +59,36 @@ switch (month) {
       throw new Error('Must provide a valid month.');
       break; 
     }
-}
+
+    let userNum = prompt('Choose 1 for rock, 2 for paper or 3 for scissors');
+    let randomNo = Math.floor(Math.random() * 3) + 1;
+
+    function compare (userNum, randomNo){
+        let rock = 1;
+        let paper = 2;
+        let scissors = 3;
+        if (userNum === randomNo) {
+            return ('The game is a tie!');
+        } 
+        if (userNum === 1) {
+            if (randomNo === 2) {
+                return 'Computer Won!';
+            } else {
+                return 'You Won!';
+            }
+        }
+        else if (userNum === 2) {
+            if (randomNo === 3) {
+                return 'Computer Won!';
+            } else {
+                return 'You Won!';
+            }
+        }
+        else if (userNum === 3) {
+            if (randomNo === 1) {
+                return 'Computer Won!'
+            } else {
+                return 'You Won!';
+            }
+        }
+    }
