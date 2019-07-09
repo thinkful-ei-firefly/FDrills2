ceca5c1e780cd1b7e64277df3ae0a20254855f80
Hector Valles <hmvalles75@yahoo.com>
2019-07-08 19:36:43 -0700

    drill2

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ function jediName (firstName, lastName) {
     return lastName.slice(0, 3) + firstName.slice(0,2)
 }
 jediName('Beyonce', 'Knowles');
+    
+
+function beyond(num) {
+   if (num === Infinity) {
+       console.log('And beyond');
+   } else if (isFinite(num) === true && num > 0) {
+       console.log('To infinity');
+   } else if (isFinite(num) === true && num < 0) {
+       console.log('To negative infinity');
+   } else {
+       console.log('Staying home');
+   }
+}
